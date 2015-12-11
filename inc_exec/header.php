dc292c60add643f96ddb07f385ee6706af2686c6
Isaac Dozier <isaactdozier@gmail.com>
2015-12-11 09:32:10 -0800

    Update bootstrap path

--- inc_exec/header.php
+++ inc_exec/header.php
@@ -8,7 +8,7 @@
     <title>Best Weed Search</title>
 
     <!-- Bootstrap -->
-    <link href="http://localhost/Strain%20Review%20Project/lib/css/bootstrap.min.css" rel="stylesheet">
+    <link href="/lib/css/bootstrap.min.css" rel="stylesheet">
 
     <!-- HTML5 shim and Respond.js for IE8 support of HTML5 elements and media queries -->
     <!-- WARNING: Respond.js doesn't work if you view the page via file:// -->
