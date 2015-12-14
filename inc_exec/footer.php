37f586bc915e6bda5025d4586135edc4e44023e4
Isaac Dozier <isaactdozier@gmail.com>
2015-12-13 23:59:58 -0800

    retry update

--- inc_exec/footer.php
+++ inc_exec/footer.php
@@ -1,13 +1,13 @@
+<div class="row">
+  <div class="col-sm-9">
+    <h3>About</h3>
     <div class="row">
-      <div class="col-sm-9">
-        <h3>About</h3>
-        <div class="row">
-          <div class="col-xs-8 col-sm-6">
-            {Social}
-          </div>
-          <div class="col-xs-4 col-sm-6">
-            {Info}
-          </div>
-        </div>
+      <div class="col-xs-8 col-sm-6">
+        {Social}
+      </div>
+      <div class="col-xs-4 col-sm-6">
+        {Info}
       </div>
     </div>
+  </div>
+</div>
