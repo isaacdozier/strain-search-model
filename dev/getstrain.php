2ca0205ba393d22a0b365cfc047a85e6a2811d3f
isaacdozier <isaactdozier@gmail.com>
2015-12-17 02:08:55 -0800

    clean dev dir
    *cleaned up files in dev directory

--- dev/getstrain.php
+++ dev/getstrain.php
@@ -1,21 +1,4 @@
-<!DOCTYPE html>
-<html>
-<head>
-<style>
-table {
-    width: 100%;
-    border-collapse: collapse;
-}
 
-table, td, th {
-    border: 1px solid black;
-    padding: 5px;
-}
-
-th {text-align: left;}
-</style>
-</head>
-<body>
 
 <?php
 $q_strain = intval($_GET['q_strain']);
@@ -25,17 +8,9 @@ include('../lib/config.php');
 $sql_strain="SELECT * FROM strain_list WHERE id = '".$q_strain."'";
 $result_strain = mysqli_query($con,$sql_strain);
 
-echo "<table>
-<tr>
-<th>Strain</th>
-</tr>";
+
 while($row_strain = mysqli_fetch_array($result_strain)) {
-    echo "<tr>";
-    echo "<td>" . $row_strain['name'] . "</td>";
-    echo "</tr>";
+    echo $row_strain['name']."<br/>";
 }
-echo "</table>";
 mysqli_close($con);
 ?>
-</body>
-</html>
